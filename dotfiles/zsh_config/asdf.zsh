4894674e1ac8e504d32817c6765392ab08cc3246
Christopher Costello <christophercostello97@gmail.com>
2023-08-22 07:41:12 -0400

    Refactor

--- dotfiles/zsh_config/asdf.zsh
+++ dotfiles/zsh_config/asdf.zsh
@@ -1,4 +1,12 @@
 if [[ -r "/opt/homebrew/opt/asdf/libexec/asdf.sh" ]]; then
     # Load ASDF
     . "/opt/homebrew/opt/asdf/libexec/asdf.sh"
+    # Allow ASDF to install dependencies to root when installing Python
+    asdf() {
+        if [[ "$1" == "install" && "$2" == "python" ]]; then
+            PIP_REQUIRE_VIRTUALENV=false command asdf "$@"
+        else
+            command asdf "$@"
+        fi
+    }
 fi
